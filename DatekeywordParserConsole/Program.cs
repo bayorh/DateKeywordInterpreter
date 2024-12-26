c9267c83150c8d456a9dcf1dfd343e4ea909c169
Toheeb BAYONLE <81241067+bayorh@users.noreply.github.com>
2024-12-26 16:03:44 +0100

    packageTester

---
+++ DatekeywordParserConsole/Program.cs
@@ -0,0 +1,44 @@
+﻿using DateTimeKeywordParser.Implementations;
+
+namespace DatekeywordParserConsole;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        var interpreter = new DateKeywordInterpreter();
+        Console.WriteLine("Date Keyword Interpreter");
+        Console.WriteLine(@"Type a date-related keyword 
+                            Keyword supported are:
+                                1. Basic: 'today', 'tomorrow', 'yesterday', 'next week', and 'last week'.
+                                2. Relatives: phrases like 'in 3 days', '5 weeks ago', 'in 2 months', and '1 year ago'.
+                                3. Now type: phrases like 'NOW-1d', 'NOW+2h'.
+                                4. Specific date pattern:  pattern like 'format(now, 'yyyy/mm/dd')' , 'format(now, 'yyyy/mm/dd')'.
+                                
+                           Type 'exit' to quit:");
+
+
+        while (true)
+        {
+            Console.Write("Input: ");
+            string input = Console.ReadLine()?.Trim().ToLower();
+
+            if (input == "exit")
+            {
+                Console.WriteLine("Exiting......!");
+                break;
+            }
+
+            try
+            {
+                string result = interpreter.Interpret(input);
+                Console.WriteLine(result);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error: {ex.Message}");
+            }
+        }
+
+     }
+}
